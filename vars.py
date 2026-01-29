74fc56b0d870528ea869f8edcf28f11105c5ef5c
Hero Sundra <jaibajarangclasses@gmail.com>
2026-01-29 10:32:53 +0530

    Change BOT_TOKEN to a new environment variable
    Updated BOT_TOKEN to a new value.

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23000618"))
 API_HASH = environ.get("API_HASH", "da88b4f9c15b2860271e086444346a93")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7554739682:AAFTz3i-007gIOzeOQV6z5NvQBH0b9NxskY")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8133691452:AAE62ABLX0DRWb82uiqT2C85ywXR9AD8CeE")
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
